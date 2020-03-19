ee441934439f6741cad32466f13910e2580765a4
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2020-03-19 18:52:02 +1100

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib-v8';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ScullyLibModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
